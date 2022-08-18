a0c1dd53b5df98447d81ebbc4cab21b996238128
Zhengyan Wu <wuzhengyan2016@gmail.com>
2022-08-18 21:55:39 +0800

    refactor: react-dom use new api

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import ReactDom from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import Logo from '@/assets/images/logo.png'
 import './index.scss'
 
-ReactDom.render((
+const root = createRoot(document.getElementById('root') as HTMLElement)
+root.render((
   <div>
     <p>Hello,This is pages changed</p>
     <img src={Logo} alt="" />
   </div>
-), document.getElementById('root'))
+))
