64302ee13713fd8019c85c436717680efbebe484
Zhengyan Wu <wuzhengyan2016@gmail.com>
2022-08-24 21:56:33 +0800

    feat: header menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,30 @@
 import React from 'react'
-import { Layout } from 'antd'
+import { Layout, Menu } from 'antd'
+import type { MenuProps } from 'antd'
 import logo from '@/assets/images/logo.png'
 import styles from './index.scss'
 
 const { Header: AHeader } = Layout
 
 export default function Header() {
+  const items: MenuProps['items'] = [
+    { key: 'home', label: '首页' },
+    { key: 'course', label: '课程' },
+    { key: 'material', label: '素材' },
+  ]
   return (
     <AHeader className={styles.header}>
       <div className={styles.container}>
-        <img src={logo} className={styles.logo} />
+        <div className={styles.logoWrapper}>
+          <img src={logo} className={styles.logo} />
+          <h2 className={styles.logoText}>HowToPr</h2>
+        </div>
+        <Menu
+          className={styles.menu}
+          mode="horizontal"
+          items={items}
+          selectedKeys={['home']}
+        />
       </div>
     </AHeader>
   )
